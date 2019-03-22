de17a84c163c117feba4bb453040ea3b64eb8188
Shayron Aguiar <shayron.aguiar@gmail.com>
2019-03-21 21:56:07 -0300

    Update main.c

--- Pilha/main.c
+++ Pilha/main.c
@@ -15,9 +15,42 @@ Pilha inicPilha() {
     return p;
 }
 
+int vaziaPilha(pPilha p) {
+    return p->topo == -1;
+}
+
 void push(pPilha p, TipoP elem) {
+    if (p->topo == MAX - 1) {
+        printf("\nPilha cheia\n");
+        return;
+    }
     p->topo++;
     p->info[p->topo] = elem;
+}
+
+TipoP pop(pPilha p) {
+    TipoP elem;
+
+    if (vaziaPilha(p)) {
+        printf("\nPilha vazia\n");
+        return;
+    }
+
+    elem = p->info[p->topo];
+    p->topo--;
+    return elem;
+}
+
+void destruirPilha(pPilha p) {
+    free(p);
+}
+
+TipoP infoPilha(pPilha p){
+    if(vaziaPilha(p)) {
+        printf("\nPilha vazia\n");
+        return;
+    }
+    return p->info[p->topo];
 }
 
 int main()
