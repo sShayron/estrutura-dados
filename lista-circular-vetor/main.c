ce34f88063ad6f3deb0e3c8471eafcb0ebc49edb
Shayron Aguiar <shayron.aguiar@gmail.com>
2019-03-28 22:12:10 -0300

    Create main.c

---
+++ lista-circular-vetor/main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 10;
+typedef int TipoF;
+
+typedef struct TFila {
+    int primeiro, ultimo;
+    TipoF info[MAX];
+} *Fila;
+
+Fila inicFila(void) {
+  Fila f = (Fila) malloc(sizeof(struct TFila));
+  f->primeiro = -1;
+  f->ultimo = -1
+  
+  return f;
+}
+void adicFila(Fila f, TipoF elem) {
+    if(f->primeiro == -1) {
+        f->primeiro = 0;
+        f->ultimo = 0;
+        f->info[0] = elem;
+    } else if((f->ultimo + 1)%MAX == f->primeiro){
+        printf("Fila cheia");
+    } else {
+        f->ultimo = (f->ultimo + 1) % MAX;
+        f->info(f->ultimo) = elem;
+    }
+}
+TipoF elimFila(Fila f) {
+    TipoF aux;
+    
+    if (f->primeiro == -1) {
+        printf("Fila vazia");
+    } else if (f->primeiro == f->ultimo) {
+        aux = f->info(f->primeiro);
+        f->primeiro = -1;
+        f->ultimo = -1;
+        return aux;
+    } else {
+        aux = f->info(f->primeiro);
+        f->primeiro = (f->primeiro + 1)%MAX;
+        return aux;
+    }
+}
+TipoF infoFila(Fila f) {
+    if(f->primeiro == -1) {        
+        printf("Fila vazia");
+    } else {
+        return f->info(f->primeiro);
+    }
+}
+int vaziaFila(Fila f) {
+    return (f->primeiro == -1);
+}
+void destruirFila(Fila f) {
+    free(f);
+}
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
