2cc488c4cd1ccb7c9d5d26a075158b0a8b1b7727
Shayron Aguiar <shayron.aguiar@gmail.com>
2019-03-28 22:24:07 -0300

    Update main.c

--- lista-circular-vetor/main.c
+++ lista-circular-vetor/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX 10;
+#define MAX 10
 typedef int TipoF;
 
 typedef struct TFila {
@@ -11,8 +11,8 @@ typedef struct TFila {
 Fila inicFila(void) {
   Fila f = (Fila) malloc(sizeof(struct TFila));
   f->primeiro = -1;
-  f->ultimo = -1
-  
+  f->ultimo = -1;
+
   return f;
 }
 void adicFila(Fila f, TipoF elem) {
@@ -24,30 +24,30 @@ void adicFila(Fila f, TipoF elem) {
         printf("Fila cheia");
     } else {
         f->ultimo = (f->ultimo + 1) % MAX;
-        f->info(f->ultimo) = elem;
+        f->info[f->ultimo] = elem;
     }
 }
 TipoF elimFila(Fila f) {
     TipoF aux;
-    
+
     if (f->primeiro == -1) {
         printf("Fila vazia");
     } else if (f->primeiro == f->ultimo) {
-        aux = f->info(f->primeiro);
+        aux = f->info[f->primeiro];
         f->primeiro = -1;
         f->ultimo = -1;
         return aux;
     } else {
-        aux = f->info(f->primeiro);
+        aux = f->info[f->primeiro];
         f->primeiro = (f->primeiro + 1)%MAX;
         return aux;
     }
 }
 TipoF infoFila(Fila f) {
-    if(f->primeiro == -1) {        
+    if(f->primeiro == -1) {
         printf("Fila vazia");
     } else {
-        return f->info(f->primeiro);
+        return f->info[f->primeiro];
     }
 }
 int vaziaFila(Fila f) {
@@ -55,6 +55,27 @@ int vaziaFila(Fila f) {
 }
 void destruirFila(Fila f) {
     free(f);
+}
+
+void printFila(Fila f) {
+    if (vaziaFila(f)) {
+        printf("Fila vazia");
+    } else {
+        Fila copia = inicFila();
+        TipoF elem;
+
+        while(!vaziaFila(f)) {
+            elem = elimFila(f);
+            printf(" %d ", elem);
+            adicFila(copia, elem);
+        }
+        printf("\n");
+
+        while(!vaziaFila(copia)) {
+            adicFila(f, elimFila(copia));
+        }
+        destruirFila(copia);
+    }
 }
 
 int main()
