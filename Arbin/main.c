9bfe2f4b216bc267c402d3f8cdcdedb607086fb3
Shayron Aguiar <shayron.aguiar@gmail.com>
2019-04-25 21:35:43 -0300

    Update main.c

--- Arbin/main.c
+++ Arbin/main.c
@@ -1,35 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-typedef int TipoA;
-
-typedef struct NoDoArbin {
-    TipoA info;
-    struct NoDoArbin *esq, *dir;
-} Tarbin, *Arbin;
-
-Arbin inicArbin() {
-}
-
-Arbin esqArbin(Arbin a) {
-}
-
-Arbin dirArbin(Arbin a) {
-}
-
-int vaziaArbin(Arbin a) {
-}
-
-int estaArbin(Arbin a, TipoA elem) {
-    if(vaziaArbin(a)) {
-        return 0;
-    }
-    if (raizArbin(a) == elem) {
-        return 1;
-    }
-    
-    return estaArbin(esqArbin(a)) || estaArbin(dirArbin(a));
-}
+#include "Arbin.c"
 
 int main()
 {
